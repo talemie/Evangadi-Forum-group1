c7893c56f9c4641b6daea841b0bc6a958a06bff2
temu man <talemie@yahoo.com>
2024-01-03 19:56:31 -0600

    border hovering effect on ask question updated

--- client/src/Components/AllQuestions/AllQuestions.jsx
+++ client/src/Components/AllQuestions/AllQuestions.jsx
@@ -31,19 +31,19 @@ function AllQuestions() {
 		<>
 			{questions?.map((question, i) => (
 				<Link to={`/question/${question.questionid}`} key={i}>
-					<div className="group each__question  border-t border-gray-300 py-4 hover:bg-slate-200   ">
-						<div className="flex justify-between pl-4 ">
+					<div className="group each__question  border-t border-gray-300 py-2 hover:bg-slate-200   ">
+						<div className="flex justify-between pl-4 pt-4">
 							<div className="flex justify-between">
 								<div className="hover:text-black w-8 h-8 mr-10">
 									<FaUserAlt className="text-6xl border border-black rounded-full p-2 group-hover:bg-black group-hover:text-white " />
 								</div>
-								<div className="ml-10">{question.title}</div>
+								<div className="ml-10 py-6 font-mono">{question.title}</div>
 							</div>
 							<div>
-								<MdNavigateNext className="text-5xl text-bold mr-5 group-hover:mr-1" />
+								<MdNavigateNext className="text-5xl text-bold mr-5 transition-all duration-500 group-hover:mr-1" />
 							</div>
 						</div>
-						<p className="pl-4 ml-3 mt-6">{question?.username}</p>
+						<p className="pl-4 ml-2 ">{question?.username}</p>
 					</div>
 				</Link>
 			))}
